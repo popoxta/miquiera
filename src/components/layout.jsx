68dbb9a7c7f2660cd28c87b04f8083e299bbe5f7
popoxta <popoxta@gmail.com>
2023-05-05 16:23:38 +1200

    home page and css styles

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,15 +1,15 @@
 import Header from "./header.jsx";
 import Footer from "./footer.jsx";
 import Cart from "./cart.jsx";
-import Home from "../pages/home.jsx";
 import Image from "./image.jsx";
+import Home from "../pages/home.jsx";
 
 export default function Layout(){
     return(
         <div className={'body'}>
             <Image />
             <Header title={false}/>
-
+            <Home />
             <Cart />
             <Footer/>
         </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,19 @@
-import Header from "../components/header.jsx";
-
-export default function Home(){
+export default function Home() {
+    return (
+        <div className={'home-content'}>
+            <div className={'home-header'}>
+                <h2>find your style</h2>
+                <h2><span>anywhere</span> you want it</h2>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae erat ac nisi imperdiet finibus
+                blandit sed metus. Ut vel tellus nisi. Nunc tempor metus sit amet justo sagittis, at scelerisque dolor
+                consequat. Duis tincidunt nisi quis est scelerisque dictum. In accumsan libero ex, sed facilisis ligula
+                dictum quis. Fusce sed tellus vel risus tincidunt viverra ut et massa. Vestibulum laoreet scelerisque
+                consectetur. Sed convallis lectus eget tellus auctor hendrerit. Duis vitae euismod lectus, et lacinia
+                tortor. Nam fringilla ullamcorper semper.
+            </p>
+            <a href={''}>shop now</a>
+        </div>
+    )
 }
